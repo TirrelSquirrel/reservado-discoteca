bf498efaa36cc0f45a696b2e864d89685d084825
BauteNonSinger <bautista19298@gmail.com>
2023-10-21 21:39:31 +0200

    Añadido cambio entre login y registro

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './Components/Styles/styles.css'
 import Login from './Components/Forms/Login';
+import Main from './Components/Container/Main';
 
 function App() {
   return (
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Login />} />
+          <Route path='' element={<Login />} />
+          <Route path='main' element={<Main />} />
         </Routes>
       </BrowserRouter>
   );
--- src/Components/Forms/Login.js
+++ src/Components/Forms/Login.js
@@ -1,29 +1,140 @@
-import React from 'react';
-import { Form, Button } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Form, Button, Alert } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-    return (
-      <div className="login-container">
-        <Form>
+  //! TEMP
+  const tempEmail = "baute@email.es";
+  const tempPass = "pass123";
+
+  //! FIN TEMP
+
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordRepeat, setPasswordRepeat] = useState("");
+
+  const [invalidCredentials, setInvalidCredentials] = useState(false);
+
+  const [registred, setRegistred] = useState(true);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log("Iniciando sesion");
+    if (email !== tempEmail || password !== tempPass) {
+      console.error("Credenciales invalidas");
+      setInvalidCredentials(true);
+
+      return;
+    }
+
+    setEmail("");
+    setPassword("");
+    setInvalidCredentials(false);
+    console.log("Sesion iniciada");
+    navigate("/main");
+  };
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+  }
+
+  return (
+    <div className="login-container">
+      {registred ? (
+        <Form onSubmit={(e) => handleLogin(e)}>
           <h2 className="login-title">Inicia sesión</h2>
 
           <div className="login-form">
             <Form.Group className="mb-3">
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="correo@email.es" />
+              <Form.Control
+                type="email"
+                placeholder="correo@ejemplo.es"
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
             </Form.Group>
-            <Form.Group className='mb-3'>
-                <Form.Label>Contraseña</Form.Label>
-                <Form.Control type='password' placeholder='Contraseña' />
+            <Form.Group className="mb-3">
+              <Form.Label>Contraseña</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Contraseña"
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
             </Form.Group>
 
-            <Button variant='primary' type='submit'>
+            <div className="login-buttons">
+              <Button
+                variant="warning"
+                onClick={() => setRegistred(!registred)}
+              >
+                Regístrate
+              </Button>
+
+              <Button variant="primary" type="submit">
                 Entrar
-            </Button>
+              </Button>
+            </div>
+            {invalidCredentials && (
+              <Alert variant="danger" className="mt-2">
+                Credenciales inválidas
+              </Alert>
+            )}
           </div>
         </Form>
-      </div>
-    );
-}
+      ) : (
+        <Form onSubmit={(e) => handleRegister(e)}>
+          <h2 className="login-title">Regístrate</h2>
+
+          <div className="login-form">
+            <Form.Group className="mb-3">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="correo@ejemplo.es"
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Contraseña</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Contraseña"
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Repite tu contraseña</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Contraseña"
+                onChange={(e) => setPasswordRepeat(e.target.value)}
+                required
+              />
+            </Form.Group>
+
+            <div className="login-buttons">
+              <Button
+                variant="warning"
+                onClick={() => setRegistred(!registred)}
+              >
+                ¿Ya tienes cuenta?
+              </Button>
+
+              <Button variant="primary" type="submit">
+                Registrar
+              </Button>
+            </div>
+          </div>
+        </Form>
+      )}
+    </div>
+  );
+};
 
 export default Login;
