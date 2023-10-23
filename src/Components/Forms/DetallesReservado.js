3669b8b2a3b0c169512dd7d16661d20d33196dbe
BauteNonSinger <bautista19298@gmail.com>
2023-10-23 20:11:55 +0200

    Añadido mapavectorial

--- src/Components/Forms/DetallesReservado.js
+++ src/Components/Forms/DetallesReservado.js
@@ -1,12 +1,26 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import maishaMap from "../../img/vectors/Untitled.svg";
 
 export default function DetallesReservado() {
   let bookPlace = useParams().bookPlace.replace("-", " ");
-  
+
+  const maps = [
+    { name: "Rock&Mola", map: undefined },
+    { name: "Maisha", map: maishaMap },
+    {name: 'Winter Club', map: undefined}
+  ];
+
+  const place = maps.find(o => o.name === bookPlace)
+
   return (
     <>
       <h3 className="book-detail-title">{bookPlace}</h3>
+      <img
+        src={place.map}
+        alt="map"
+        className="map-zone-img"
+      />
     </>
   );
 }
