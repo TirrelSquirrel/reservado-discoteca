14f86af9669958b2edc7fbfd76ed10285301cef8
BauteNonSinger <bautista19298@gmail.com>
2023-10-24 14:10:13 +0200

    WIP bottles

--- src/Components/Pure/BottlesList.js
+++ src/Components/Pure/BottlesList.js
@@ -1,20 +1,30 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function BottlesList() {
+  const [bottles, setBottles] = useState([]);
 
-    const [bottles, setBottles] = useState([])
+  useEffect(() => {
+    axios.get("http://localhost:5000/drink").then((data) => {
+      setBottles(data.data);
+    });
+  }, []);
 
-    useEffect(() => {
-      axios.get('http://localhost:5000/drink')
-        .then((data) => {
-            setBottles(data.data)
-        })
-    }, [])
-    
   return (
-    <div>{bottles.map((bottle, index) => {
-        return ( <div key={index}>{bottle.name}</div>)
-    })}</div>
-  )
+    <div className="generic-container">
+      <div className="main-container">
+        {bottles.map((bottle, index) => {
+          return (
+            <div className="event-item" key={index}>
+              <p className="event-name">
+                {bottle.name} {bottle.cuantity}L
+              </p>
+              <img src="undefined" alt={"img " + bottle.name + " img"} />
+              <p className="event-name">{bottle.price}€</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
