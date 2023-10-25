0dc5bd05bdf562a5d80772a74ed6565cdaefe1aa
BauteNonSinger <bautista19298@gmail.com>
2023-10-25 14:32:15 +0200

    Añadido recogida de datos de FAQs

--- src/Components/Pure/FAQ.js
+++ src/Components/Pure/FAQ.js
@@ -1,18 +1,32 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import BackButton from './BackButton'
 
 export default function FAQ() {
 
-  const [faq, setFaq] = useState([]);
+  const [faqList, setFaqList] = useState([]);
 
   useEffect(() => {
-    
+    axios.get('http://localhost:5000/faq')
+      .then((data) => {        
+        setFaqList(data.data)
+        console.log("FAQs", faqList);
+      })
   }, []);
 
   return (
     <div className="generic-container">
       <div className="main-container">
         <h2 className="bold">Preguntas frecuentes</h2>
+        {faqList.map((faq, index) => {
+          return (
+            <div className="event-item" key={index}>
+              <p className="event-name">{faq.question}</p>
+              <p className="event-description">{faq.answer}</p>
+            </div>
+          )
+        })}
+        <BackButton />
       </div>
     </div>
   );
