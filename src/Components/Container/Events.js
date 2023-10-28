19405e02c26b92369cb39c7cc803b25721c272e8
BauteNonSinger <bautista19298@gmail.com>
2023-10-28 23:01:12 +0200

    Movido el formateo de fecha al back

--- src/Components/Container/Events.js
+++ src/Components/Container/Events.js
@@ -11,14 +11,6 @@ const Events = () => {
       .then((res) => seteventsArray(res.data))
   }, []);
 
-  const formatDate = (date) => {
-    let dateArray = date.split('-')
-    dateArray[2] = dateArray[2].split('T')
-    dateArray[2] = dateArray[2][0]
-    dateArray.reverse()
-    let dateString = dateArray.join('-')
-    return dateString;
-  }
   return (
     <div className="generic-container">
       <div className="main-container">
@@ -27,7 +19,7 @@ const Events = () => {
             <div className="event-item px-2" key={event._id}>
               <p className="event-name">{event.title}</p>
               <p className="event-description">{event.description}</p>
-              <p className="event-date">{formatDate(event.date)}</p>
+              <p className="event-date">{event.date}</p>
             </div>
           );
         })}
